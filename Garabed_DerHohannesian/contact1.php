68856941329bc20c6173da953433ab02fe4859ca
Dwight Norris <norrisdb@yahoo.com>
2015-09-11 20:33:53 -0400

    Initialize Repository

---
+++ Garabed_DerHohannesian/contact1.php
@@ -0,0 +1,17 @@
+﻿<?php
+	$name=$_REQUEST['inputName'];
+    $email=$_REQUEST['inputEmail'];
+    $message=$_REQUEST['textArea'];
+    if (($name=="")||($email=="")||($message==""))
+		{
+			echo "All fields are required, please fill out the form again.";
+        }
+        
+		else {
+			$from="From: $name
+            \r\nReturn-path: $email";
+            $subject="Message sent using your contact form";
+            mail("webmaster@garabedderhohannesian.com", $subject, $message, $from);
+            echo "Email sent!";
+		}
+?>
