c7b4ffeeb5ced1cf2c28d94cee2bc7d0038a1e8c
Dwight Norris <norrisdb@yahoo.com>
2015-09-13 23:14:45 -0400

    Finalized Contact webpage

--- Garabed_DerHohannesian/contact.php
+++ Garabed_DerHohannesian/contact.php
@@ -1,5 +1,85 @@
-﻿<?php  
-    include 'contact_info.php';
+﻿<?php
+    if (isset($_POST["submit1"])) {
+        $name1 = $_POST['name1'];
+        $email1 = $_POST['email1'];
+        $message1 = $_POST['message1'];
+        $human1 = intval($_POST['human1']);
+        $from1 = 'Contact Form'; 
+        $to1 = 'info@garabedderhohannesian.com'; 
+        $subject1 = 'GarabedDerHohannesian.com Contact Request ';
+        
+        $body1 = "From: $name1\n E-Mail: $email1\n Message:\n $message1";
+ 
+        // Check if name has been entered
+        if (!$_POST['name1']) {
+            $errName1 = 'Please enter your name';
+        }
+        
+        // Check if email has been entered and is valid
+        if (!$_POST['email1'] || !filter_var($_POST['email1'], FILTER_VALIDATE_EMAIL)) {
+            $errEmail1 = 'Please enter a valid email address';
+        }
+        
+        //Check if message has been entered
+        if (!$_POST['message1']) {
+            $errMessage1 = 'Please enter your message';
+        }
+        //Check if simple anti-bot test is correct
+        if ($human1 !== 11) {
+            $errHuman1 = 'Your anti-spam answer is incorrect';
+        }
+ 
+// If there are no errors, send the email
+if (!$errName1 && !$errEmail1 && !$errMessage1 && !$errHuman1) {
+    if (mail ($to1, $subject1, $body1, $from1)) {
+        $result1='<div class="alert alert-success">Thank You! We will respond shortly</div>';
+    } else {
+        $result1='<div class="alert alert-danger">We are sorry there was an error sending your message. Please try again</div>';
+    }
+}
+    }
+?>
+
+<?php
+    if (isset($_POST["submit2"])) {
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+        $message = $_POST['message'];
+        $human = intval($_POST['human']);
+        $from = 'Contact Form'; 
+        $to = 'webmaster@garabedderhohannesian.com'; 
+        $subject = 'GarabedDerHohannesian.com Contact Request ';
+        
+        $body = "From: $name\n E-Mail: $email\n Message:\n $message";
+ 
+        // Check if name has been entered
+        if (!$_POST['name']) {
+            $errName = 'Please enter your name';
+        }
+        
+        // Check if email has been entered and is valid
+        if (!$_POST['email'] || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+            $errEmail = 'Please enter a valid email address';
+        }
+        
+        //Check if message has been entered
+        if (!$_POST['message']) {
+            $errMessage = 'Please enter your message';
+        }
+        //Check if simple anti-bot test is correct
+        if ($human !== 11) {
+            $errHuman = 'Your anti-spam answer is incorrect';
+        }
+ 
+// If there are no errors, send the email
+if (!$errName && !$errEmail && !$errMessage && !$errHuman) {
+    if (mail ($to, $subject, $body, $from)) {
+        $result='<div class="alert alert-success">Thank You! We will respond shortly</div>';
+    } else {
+        $result='<div class="alert alert-danger">We are sorry there was an error sending your message. Please try again</div>';
+    }
+}
+    }
 ?>
 
 <!DOCTYPE html>
@@ -30,7 +110,7 @@
                     <li><a href="paintings.html">Paintings</a></li>
                     <li><a href="reviews.html">Reviews</a></li>
                     <li><a href="blog.html">Blog</a></li>
-                    <li class="active"><a href="contact.html">Contact</a></li>
+                    <li class="active"><a href="contact.php">Contact</a></li>
 
                 </ul>
             </div>
@@ -46,6 +126,52 @@
                 <form class="form-horizontal" role="form" method="post">
                     <fieldset>
                         <legend>Artwork Questions</legend>
+                        <div class="form-group">
+                            <label for="name1" class="col-sm-2 control-label">Name</label>
+                            <div class="col-sm-10">
+                                <input type="text" class="form-control" id="name" name="name1" placeholder="First & Last Name" value="<?php echo htmlspecialchars($_POST['name1']); ?>">
+                                <?php echo "<p class='text-danger'>$errName1</p>";?>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="email1" class="col-sm-2 control-label">Email</label>
+                            <div class="col-sm-10">
+                                <input type="email" class="form-control" id="email1" name="email1" placeholder="example@domain.com" value="<?php echo htmlspecialchars($_POST['email1']); ?>">
+                                <?php echo "<p class='text-danger'>$errEmail1</p>";?>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="message1" class="col-sm-2 control-label">Message</label>
+                            <div class="col-sm-10">
+                                <textarea class="form-control" rows="4" name="message1"><?php echo htmlspecialchars($_POST['message1']);?></textarea>
+                                <?php echo "<p class='text-danger'>$errMessage1</p>";?>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="human1" class="col-sm-2 control-label">7 + 4 = ?</label>
+                            <div class="col-sm-10">
+                                <input type="text" class="form-control" id="human1" name="human1" placeholder="Your Answer">
+                                <?php echo "<p class='text-danger'>$errHuman1</p>";?>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <div class="col-sm-10 col-sm-offset-2">
+                                <input id="submit1" name="submit1" type="submit" value="Submit" class="btn btn-primary">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <div class="col-sm-10 col-sm-offset-2">
+                                <?php echo $result1; ?>    
+                            </div>
+                        </div>
+                    </fieldset>
+                </form>   
+            </div>
+			
+			<div class="col-md-6 col-sm-6">
+                <form class="form-horizontal" role="form" method="post">
+                    <fieldset>
+                        <legend>Technical Issues</legend>
                         <div class="form-group">
                             <label for="name" class="col-sm-2 control-label">Name</label>
                             <div class="col-sm-10">
@@ -76,7 +202,7 @@
                         </div>
                         <div class="form-group">
                             <div class="col-sm-10 col-sm-offset-2">
-                                <input id="submit" name="submit" type="submit" value="Submit" class="btn btn-primary">
+                                <input id="submit2" name="submit2" type="submit" value="Submit" class="btn btn-primary">
                             </div>
                         </div>
                         <div class="form-group">
