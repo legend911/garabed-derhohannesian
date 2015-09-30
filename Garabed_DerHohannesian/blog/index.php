f42c328138ee374d39ad8b54b43250dfd8f16aee
Dwight Norris <norrisdb@yahoo.com>
2015-09-29 23:31:41 -0400

    installed Wordpress Blog and updated all webpage links

---
+++ Garabed_DerHohannesian/blog/index.php
@@ -0,0 +1,16 @@
+<?php
+/* Short and sweet */
+define('WP_USE_THEMES', true);
+require('./wp-blog-header.php');
+?>
+
+
+
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-67685422-1', 'auto');
+  ga('send', 'pageview');
